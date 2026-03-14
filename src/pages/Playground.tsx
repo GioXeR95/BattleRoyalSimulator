85bef5ac7b8646fa8413638d4b73fcdda6a0ada6
AliBi <bivona.alice@gmail.com>
2026-03-14 17:52:05 +0100

    Add playground page

---
+++ src/pages/Playground.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Playground: React.FC = () => {
+  return (
+    <div>
+      <h1>I WANT TO DIE</h1>
+      <h2>PLEASE HELP ME</h2>
+    </div>
+  );
+};
+
+export default Playground;
