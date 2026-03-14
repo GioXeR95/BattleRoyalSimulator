f15d58bb673142bc2aa6f0fd4d62d8461be0828c
GioXeR95 <gioele.95@gmail.com>
2026-03-14 12:27:51 +0100

    Move Sass static tokens in variables. Example files. Build script.

---
+++ src/components/ImagePreview.tsx
@@ -0,0 +1,9 @@
+// THIS IS AN EXAMPLE FILE OF A COMPONENT.
+// THIS FILE IS NOT USED IN THE APP
+// THIS FILE CAN BE DELETED IN THE FUTURE
+
+import '../styles/components/imagePreview.scss';
+
+export function ImagePreview() {
+  return <div className="image-preview">...</div>
+}
